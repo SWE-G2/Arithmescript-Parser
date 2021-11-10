3b0e8a4e636e00259a9291abd1e5330bdaad691e
Chris D (hare_ware) <cheesecakecgcontact@gmail.com>
2021-11-09 19:15:16 -0500

    Recommented some broken code. +1 technical debt

--- pkg/Arithmescript-Parser/parser.go
+++ pkg/Arithmescript-Parser/parser.go
@@ -26,33 +26,33 @@ type Token struct {
 // Maybe we should drop tokenization? I don't think we actually need it unless we 
 // make more than one target language.
 
-func parseStringByWords(asMarkup string, grammar *Grammar) (parsedTokens []Token, error) {
-	words := strings.Split(asMarkup, " ")
-	currToken := Token
-	hasFoundStart := false
-	hasFoundEnd := false
-	hasFoundBody := false
-	hasFoundDelimiter := false
-	for wordIndex, word := words {
+// func parseStringByWords(asMarkup string, grammar *Grammar) (parsedTokens []Token, error) {
+// 	words := strings.Split(asMarkup, " ")
+// 	currToken := Token
+// 	hasFoundStart := false
+// 	hasFoundEnd := false
+// 	hasFoundBody := false
+// 	hasFoundDelimiter := false
+// 	for wordIndex, word := words {
 
-		for _, rule := range grammar.rules {
-			if strings.Contains(word, rule.asStart) {
-				Token.grammar = &rule
-				parsedTokens = append(parsedTokens, t)
-				continue
+// 		for _, rule := range grammar.rules {
+// 			if strings.Contains(word, rule.asStart) {
+// 				Token.grammar = &rule
+// 				parsedTokens = append(parsedTokens, t)
+// 				continue
 
-			}
-			if strings.Contains(word, rule.asDelimiter) {
+// 			}
+// 			if strings.Contains(word, rule.asDelimiter) {
 
-				continue
-			}
-		}
-		if hasFoundStart && hasFoundEnd && !hasFoundDelimiter {
-			currToken = words[wordIndex - 1]
-		}
-	}
-	return parsedTokens, nil
-}
+// 				continue
+// 			}
+// 		}
+// 		if hasFoundStart && hasFoundEnd && !hasFoundDelimiter {
+// 			currToken = words[wordIndex - 1]
+// 		}
+// 	}
+// 	return parsedTokens, nil
+// }
 
 func parseString(asMarkup string, grammar *Grammar) (s string, err error) {
 	words := strings.Split(asMarkup, " ")
