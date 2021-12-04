7f2c802abe50c725648dd3436b880671358aea96
Chris D (hare_ware) <cheesecakecgcontact@gmail.com>
2021-12-03 19:42:36 -0500

    Added binding

--- bindings/js/js.go
+++ bindings/js/js.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"syscall/js"
+
+	asp "github.com/SWE-G2/Arithmescript-Parser/parser"
 )
 
 
@@ -15,12 +17,32 @@ func sayHello(self js.Value, args []js.Value) interface{} {
 	return nil
 }
 
+// // Parse a string with ASGRAMMER
+// func ParseMultilineASG(self js.Value, args []js.Value) interface{} {
+// 	asp.ParseMultiline(args[0].String(), asp.ASGRAMMAR)
+// 	return nil
+// }
+
+// Convert AS to Latex
+func ConvertASToLatex(self js.Value, args []js.Value) interface{} {
+	tokens, err := asp.ParseMultiline(args[0].String(), asp.ASGRAMMAR)
+	if err != nil {
+		return err
+	}
+	var latexString string
+	for _, t := range tokens {
+		latexString += asp.LatexConversionTable.Convert(t)
+	}
+	return latexString
+}
+
 func main() {  
     fmt.Println("Go Web Assembly")
-    js.Global().Set("sayHello", js.FuncOf(sayHello))
+    // js.Global().Set("ParseMultilineASG", js.FuncOf(ParseMultilineASG))
+    js.Global().Set("ConvertASToLatex", js.FuncOf(ConvertASToLatex))
 	<-make(chan bool)	// Stops program from exiting, 
 						// makes go wait for this channel to exit 
-						// (it never will)
+						// (it never will, muhuhahahaha!)
 }
 // Use this to compile for WASM
 // GOOS=js GOARCH=wasm go build -o asparser.wasm
